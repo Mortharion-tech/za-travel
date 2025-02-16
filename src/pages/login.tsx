43ba2ae0c21c7869c4ad4689b22daf98e2c6c906
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-16 22:54:34 +0100

    Added Login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,10 @@
+import { Button, Typography } from '@mui/material';
+
 export default function LoginPage() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Typography variant="h1">Login</Typography>
+      <Button variant="contained">Login</Button>
+    </div>
+  );
 }
