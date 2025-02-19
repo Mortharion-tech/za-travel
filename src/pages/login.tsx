fc8c390265506dcf6381cb3d21b8af2aa3a5559f
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-19 22:32:01 +0100

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,18 @@
-import LoginForm from '@features/auth/LoginForm';
+import { Typography } from '@mui/material';
+
+import { APP_NAME } from '@config/constants';
+import LoginForm from '@features/auth/components/LoginForm';
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <>
+      <Typography component="h1" variant="h2" mb={1}>
+        Login
+      </Typography>
+      <Typography color="text.secondary" mb={4}>
+        Login to access {APP_NAME}
+      </Typography>
+      <LoginForm />
+    </>
+  );
 }
