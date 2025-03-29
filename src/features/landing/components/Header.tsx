1f264c3e14544661e35635d4036b3579766fc371
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-03-28 23:35:09 -1200

    Create Header.tsx

---
+++ src/features/landing/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Box, Stack } from '@mui/material';
+
+import AppButton from '@features/ui/AppButton';
+import Logo from '@features/ui/logo/Logo';
+
+export default function Header() {
+  return (
+    <>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Box>
+          <Logo />
+        </Box>
+        <Stack direction="row" gap={2.5}>
+          <AppButton>Log in</AppButton>
+          <AppButton>Sign up</AppButton>
+        </Stack>
+      </Stack>
+    </>
+  );
+}
