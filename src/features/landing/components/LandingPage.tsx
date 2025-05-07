0e6f16b3b85e0298e366656cac17b222c1f400e6
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-05-08 00:41:07 +0200

    Update LandingPage.tsx

--- src/features/landing/components/LandingPage.tsx
+++ src/features/landing/components/LandingPage.tsx
@@ -1,7 +1,9 @@
+import Advantages from './Advantages';
 import AppFeatures from './AppFeatures';
 import Footer from './Footer';
 import Header from './Header';
 import Hero from './Hero/Hero';
+import NextTrip from './NextTrip';
 
 export default function LandingPage() {
   return (
@@ -9,6 +11,8 @@ export default function LandingPage() {
       <Header />
       <Hero />
       <AppFeatures />
+      <Advantages />
+      <NextTrip />
       <Footer />
     </>
   );
