cfa6fe62f77a5620853760f4c1bdf101c5d6ddb1
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-05-12 22:50:53 -0300

    Update Header.tsx

--- src/features/landing/components/Header.tsx
+++ src/features/landing/components/Header.tsx
@@ -6,7 +6,7 @@ import Logo from '@features/ui/logo/Logo';
 
 export default function Header() {
   return (
-    <Container maxWidth="lg" sx={{ py: 8 }}>
+    <>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
         <Box>
           <Logo />
@@ -30,6 +30,6 @@ export default function Header() {
           </AppButton>
         </Stack>
       </Stack>
-    </Container>
+    </>
   );
 }
