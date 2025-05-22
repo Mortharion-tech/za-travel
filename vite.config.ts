6cbfe959dcc0a7b664c8744125dfac32287acd77
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-05-22 12:49:10 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       '@pages': path.resolve(__dirname, './src/pages'),
       '@features': path.resolve(__dirname, './src/features'),
       '@config': path.resolve(__dirname, './src/app/config'),
-      '@store': path.resolve(__dirname, './src/app/store'),
-      '@services': path.resolve(__dirname, './src/app/services'),
+      '@store': path.resolve(__dirname, './src/app/store'), // Removed space
+      '@services': path.resolve(__dirname, './src/app/services'), // Removed space
       '@hooks': path.resolve(__dirname, './src/app/hooks'),
     },
   },
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: 'dist',
+  },
 });
