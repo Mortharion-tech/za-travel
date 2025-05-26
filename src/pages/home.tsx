d1a4f0e9f614b3310aae50427abe989d06e315e7
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-05-26 20:28:44 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,11 @@
-import LandingPage from '@features/landing/components/LandingPage';
+import DesktopLandingPage from '@features/landing/components/DesktopLandingPage';
+import MobileLandingPage from '@features/landing/components/MobileLandingPage';
+import { useBreakpoints } from '@hooks/useBreakpoints';
 
 export default function HomePage() {
-  return <LandingPage />;
+  const { lg } = useBreakpoints();
+  if (lg) {
+    return <DesktopLandingPage />;
+  }
+  return <MobileLandingPage />;
 }
