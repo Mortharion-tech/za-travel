9cf9bff726d44723d8c9498ba1fbff686e75bbb6
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-05-31 22:35:48 -1200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,24 @@
+import { motion, useScroll } from 'motion/react';
+
 import DesktopLandingPage from '@features/landing/components/DesktopLandingPage';
 import MobileLandingPage from '@features/landing/components/MobileLandingPage';
 import { useBreakpoints } from '@hooks/useBreakpoints';
 
 export default function HomePage() {
   const { lg } = useBreakpoints();
+  const { scrollYProgress } = useScroll();
+
   if (lg) {
-    return <DesktopLandingPage />;
+    return (
+      <>
+        <motion.div style={{ scaleX: scrollYProgress }} />
+        <DesktopLandingPage />
+      </>
+    );
   }
-  return <MobileLandingPage />;
+  return (
+    <motion.div style={{ scaleX: scrollYProgress }}>
+      <MobileLandingPage />
+    </motion.div>
+  );
 }
